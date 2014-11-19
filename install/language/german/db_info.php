dffa7067ae8491def9af49cc1d3795b402def246
jChriz17 <fatandthinenter@aol.de>
2014-11-19 18:25:33 +0100

    Create db_info.php

---
+++ install/language/german/db_info.php
@@ -0,0 +1,13 @@
+<?php
+//Refuses Direct Access
+if(!defined("PhentomCMS")){ exit; };
+
+$lang['title'] = "DB Info";
+$lang['db_host'] = "DB Host";
+$lang['db_user'] = "DB Benutzername";
+$lang['db_pass'] = "DB Passwort";
+$lang['db_connect_fail'] = "Bei der Verbindung zur Datenbank ist ein Fehler aufgetreten.";
+$lang['complete_fields'] = "Alle Felder ausfüllen.";
+$lang['pass_mismatch'] = "Passwörter stimmen nicht überein.";
+
+?>
