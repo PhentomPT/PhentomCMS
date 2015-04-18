e526c5bd6129ecce5be094357501e308ce83ffd8
6kmp9 <n2o_06@hotmail.com>
2015-04-18 03:18:07 +0200

    Part 2 Basic Templating

--- core/lib/layout.php
+++ core/lib/layout.php
@@ -7,6 +7,12 @@
 		
 		public 	$globalSettings = array();
 		
+		// $layout name of the theme
+		// $globalSettings should be included 
+		// Using globalsettings for language is possible
+		
+		// Template tags/blocks syntax {% tagname %}
+		
 		public function __construct($layout='default', $globalSettings = array())
 		{
 			$template = file_get_contents('../../content/styles/'.$layout.'layout.html');
@@ -22,7 +28,8 @@
 			}
 		}
 		
-		function getBlocks()
+		// Find all the basic blocks in the template 
+		private function getBlocks()
 		{
 			preg_match_all("#{% ([^`]*?) %}#s", $this->layout, $match);
 			if($match)
@@ -34,7 +41,8 @@
 			}
 		}
 		
-		function setTag($tag, $content)
+		// Pages, modules and any other extension can have access to layout tags
+		public function setTag($tag, $content)
 		{
 			if (isset($this->tags[$tag]) || array_key_exists($tag, $this->tags))
 			{
@@ -42,7 +50,8 @@
 			}
 		}
 		
-		function setGlobals()
+		// Global information that should allways be avalible
+		private function setGlobals()
 		{
 			foreach($this->globalSettings as $key => $val)
 			{
@@ -53,7 +62,8 @@
 			}
 		}
 		
-		function printBlock()
+		//When everything is done return the layout and print it to the user
+		public function printBlock()
 		{
 			return $this->layout;
 		}
