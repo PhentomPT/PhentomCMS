dfd4a1e1e70fa82138e9557eba202c26a939864b
Phentom <PhentomPT@users.noreply.github.com>
2015-05-20 10:01:36 +0100

    Fix

--- admin/plugin/login/controller/arcemu.php
+++ admin/plugin/login/controller/arcemu.php
@@ -5,8 +5,8 @@ if (!defined("SSC")){ exit("You don't have access to this file"); }
 $username = $_POST['username'];
 $password = $common->encryptSha1($username,$_POST['password']);
 
-$db->SelectDb("auth");
-$check_account = $db->SimpleQuery("SELECT * FROM `accounts` WHERE login='$username' and encrypted_password='$password'");
+$db->SelectDb("logon");
+$check_account = $db->SimpleQuery("SELECT * FROM accounts WHERE login='$username' AND encrypted_password='$password' AND gm='az'");
 
 if (count($check_account) > 0){
 	$_SESSION['uadmin'] = $check_account[0]['username'];
