a5a2032b55b0816c27727d0f0faef41e6a757fbc
Phentom <PhentomPT@users.noreply.github.com>
2015-05-20 09:53:35 +0100

    Fix and added support for mangos

--- admin/plugin/login/index.php
+++ admin/plugin/login/index.php
@@ -1,8 +1,16 @@
 <?php
+//Refuses direct access
+if (!defined("SSC")){ exit("You don't have access to this file"); }
 
-if ($core=="arcemu"){
+switch ($core){
+case "arcemu":
 	include ("controller/arcemu.php");
-}
-else{
+	break;
+case "trinity":
+case "trinity_v6":
 	include ("controller/trinity.php");
+	break;
+case "mangos":
+	include ("controller/mangos.php");
+	break;
 }
