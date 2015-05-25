4ba7276a1d0b98f94b12eb39fc538d1418b23166
Phentom <PhentomPT@users.noreply.github.com>
2015-05-25 09:50:01 +0100

    New tokens

--- install/language/pt/install.php
+++ install/language/pt/install.php
@@ -23,8 +23,9 @@ $lang['error_in'] = "Erro na";
 $lang['query'] = "Query";
 $lang['no_error'] = "Nenhum erro foi detectado durante a instalação!";
 
-$lang['php_error'] = "Mysql 5.4 ou superior é necessário";
-$lang['permissions'] = "Dar permissões a pasta do projecto";
+$lang['php_error'] = "PHP 5.4 ou superior é necessário.";
+$lang['permissions_error'] = "Dar permissões a pasta do projecto.";
+$lang['mysql_error'] = "Mysql 5.4 ou superior é necessário."
 
 foreach ($lang as $key=>$value){
 	$system->assign($key, $lang[$key]);
